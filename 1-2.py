8c146a041211fd6f568d3159bbdfd97f27877800
mil-py <mytechnicalbox@gmail.com>
2024-10-15 07:37:33 +0300

    Add files via upload

---
+++ 1-2.py
@@ -0,0 +1,8 @@
+homework_number = 12
+work_hours = 1.5
+course_name = "Python"
+time_per_homework = work_hours / homework_number
+print('Курс: ',course_name,
+      ", всего задач: ",homework_number,
+      ", затрачено часов: ",work_hours,
+      ", среднее время выполнения: ",time_per_homework)
