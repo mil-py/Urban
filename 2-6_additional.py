f6cd316dad5a26dcc6ae64ad0f174e8719bfc141
mil-py <mytechnicalbox@gmail.com>
2024-10-26 11:51:18 +0300

    added 2-6(additional)

---
+++ 2-6_additional.py
@@ -0,0 +1,8 @@
+n = int(input("Введите число от 3 до 20:"))
+result=''
+for i in range(1,n):
+    for j in range (i+1, n):
+        if n % (i+j) == 0:
+            result+= str(i) + str(j)
+
+print('result:',result)
