d057d43a596f72dcf3c90a296ffbc654b3180bab
mil-py <mytechnicalbox@gmail.com>
2024-11-12 11:53:00 +0300

    4-1 with modules added

---
+++ module_4_1.py
@@ -0,0 +1,18 @@
+from true_math_4_1 import divide as div_t
+from fake_math_4_1 import divide as div_f
+
+result1 = div_f(69, 3)
+
+result2 = div_f(3, 0)
+
+result3 = div_t(49, 7)
+
+result4 = div_t(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
