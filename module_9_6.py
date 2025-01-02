1e09552a20546dcb995e9b2389e92536922fe2fd
mil-py <mytechnicalbox@gmail.com>
2025-01-02 15:36:00 +0300

    9_6 modified

--- module_9_6.py
+++ module_9_6.py
@@ -1,9 +1,8 @@
 def all_variants(text):
     for length in range(1, len(text) + 1):
         for start_pos in range(0, len(text)):
-            if start_pos + length > len(text):
-                continue
-            yield text[start_pos:start_pos + length]
+            if start_pos + length <= len(text):
+                yield text[start_pos:start_pos + length]
 
 
 a = all_variants("abc")
